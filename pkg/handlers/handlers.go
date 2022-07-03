59336918245332acafc3036b35e5c7d041345439
Jon Fingas <jon.fingas@gmail.com>
2022-07-03 16:14:45 -0500

    Switched to template files and created template cache

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,18 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Random-7/GoCon/pkg/render"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.go.tmpl")
+	fmt.Println("Hit Home")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.go.tmpl")
+	fmt.Println("Hit About")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,68 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	ParsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := ParsedTemplate.Execute(w, nil)
+	fmt.Println("Hit Render Template")
+
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Error parsing template", err)
-		return
+		log.Fatal(err)
 	}
+	fmt.Println(tc)
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Template not found: ", tmpl, " : ", err)
+	}
+
+	buffer := new(bytes.Buffer)
+	_ = t.Execute(buffer, nil)
+
+	_, err = buffer.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser: ", err)
+	}
+}
+
+//CreateTemplateCache creates a template cache into a map based on templates
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	fmt.Println("Hit CreateTemplateCache")
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.go.tmpl")
+	if err != nil {
+		return cache, err
+	}
+
+	for _, page := range pages {
+		fmt.Println("processing: ", page)
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return cache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.go.tmpl")
+		if err != nil {
+			return cache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.go.tmpl")
+			if err != nil {
+				return cache, err
+			}
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
