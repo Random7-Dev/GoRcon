cddb8b4f36204d8e6c6ef4d98bf1f5f6f3ea5705
Jon F <jon.fingas@random7.net>
2022-07-10 23:54:55 -0500

    fixed some stuff

--- pkg/rcon/rcon.go
+++ pkg/rcon/rcon.go
@@ -53,14 +53,13 @@ func (r *Connection) GetPlayers() (int, []string, error) {
 		fmt.Println("Error with send command: ", err)
 		return 0, nil, err
 	}
+
 	resp := strings.Split(response, ":")   // split at colon "There are 2/20 players online:Random777, Dude1872"
 	players := strings.Split(resp[1], ",") //split at comma "Random777, Dude1872"
 
 	if len(resp[1]) == 0 {
-		fmt.Println("GetPlayers returns: Count = ", 0, "Players = ", nil, "Error = ", nil)
 		return 0, nil, nil
 	}
-	fmt.Println("GetPlayers returns: Count = ", len(players), "Players = ", players, "Error = ", nil)
 	return len(players), players, nil
 }
 
