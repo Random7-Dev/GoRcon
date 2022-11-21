2ade5f54dd539065ef3d1ec4a37c02b287d50a1f
Jon fingas <jon.fingas@gmail.com>
2022-11-21 17:12:07 -0600

    basic pages made, players page pulls from rcon, links and nav bar

--- app/server/get-handlers.go
+++ app/server/get-handlers.go
@@ -18,10 +18,29 @@ func PlayersHandler(c *fiber.Ctx) error {
 	msg, _ = rcon.GetPlayers()
 	return c.JSON(msg)
 }
-
 func IndexHandler(c *fiber.Ctx) error {
+	return c.Render("pages/commands", model.TempalteData{
+		Title: "GoRcon",
+	}, "layouts/main")
+}
+func PlayersPageHandler(c *fiber.Ctx) error {
+	players, err := rcon.GetPlayers()
+	if err != nil {
+		return err
+	}
 
-	return c.Render("pages/players", fiber.Map{
-		"Title": "Hello, World!",
-	})
+	data := make(map[string]interface{})
+	data["Players"] = players
+
+	return c.Render("pages/players", model.TempalteData{
+		Title: "Players",
+		Data:  data,
+	}, "layouts/main")
+}
+
+func CommandsHandler(c *fiber.Ctx) error {
+
+	return c.Render("pages/commands", model.TempalteData{
+		Title: "Commands",
+	}, "layouts/main")
 }
