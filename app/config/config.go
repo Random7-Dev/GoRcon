40589bad0765c6d1f248e2aadda5fbc523990acb
Jon fingas <jon.fingas@gmail.com>
2022-11-27 13:42:41 -0600

    Moved setupapp with json or cmd line to config.app as reciever funcs

--- app/config/config.go
+++ app/config/config.go
@@ -1,6 +1,12 @@
 package config
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	mcrcon "github.com/Kelwing/mc-rcon"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -36,4 +42,43 @@ type DbSettings struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
 	DbName   string `json:"dbname"`
+}
+
+func (App *App) SetupAppConfig() {
+
+	flag.BoolVar(&App.Config, "config", false, "Set true to use a config.json, false to use only cmd ling args")
+
+	flag.StringVar(&App.WebSettings.Ip, "webip", "0.0.0.0", "Set the listening IP of the web server.")
+	flag.StringVar(&App.WebSettings.Port, "webport", "8080", "Set the listening Port of the web server.")
+
+	flag.StringVar(&App.RconSettings.Ip, "rconip", "127.0.0.1", "Set the IP of the RCON server to connect to.")
+	flag.StringVar(&App.RconSettings.Port, "rconport", "25575", "Set the port of the RCON server to connect to.")
+	flag.StringVar(&App.RconSettings.Password, "rconpass", "super_secret", "Set the Password of the RCON server.")
+
+	flag.StringVar(&App.DbSettings.Host, "dbhost", "127.0.0.1", "Set the hostname of the DB server to connect to.")
+	flag.StringVar(&App.DbSettings.Port, "dbport", "5432", "Set the Port of the DB server to connect to.")
+	flag.StringVar(&App.DbSettings.User, "dbuser", "postgres", "Set the username of the DB server to connect to.")
+	flag.StringVar(&App.DbSettings.Password, "dbpass", "postgres", "Set the password of the DB server to connect to.")
+	flag.StringVar(&App.DbSettings.DbName, "dbname", "gorcon", "Set the name of the database on the DB server to connect to.")
+
+	flag.Parse()
+
+	if !App.Config {
+		fmt.Println("Using Command Line args to configure app.")
+	} else {
+		fmt.Println("Using Config.json to configure app.")
+		App.setupJsonAppSettings()
+	}
+}
+
+func (App *App) setupJsonAppSettings() {
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer configFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(configFile)
+
+	json.Unmarshal(byteValue, &App)
 }
--- app/main.go
+++ app/main.go
@@ -1,12 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-
 	"github.com/Random7-JF/go-rcon/app/config"
 	"github.com/Random7-JF/go-rcon/app/model"
 	"github.com/Random7-JF/go-rcon/app/rcon"
@@ -16,11 +10,7 @@ import (
 var App config.App
 
 func main() {
-	setupAppConfig()
-
-	fmt.Println(App.WebSettings)
-	fmt.Println(App.RconSettings)
-	fmt.Println(App.DbSettings)
+	App.SetupAppConfig()
 
 	go setupDB()
 	go setupRcon()
@@ -37,44 +27,4 @@ func setupRcon() {
 	rcon.SetupConnection(&App)
 	rconsession := rcon.SetupRconSession(&App)
 	rcon.NewRconSession(rconsession)
-}
-
-func setupAppConfig() {
-	flag.BoolVar(&App.Config, "config", false, "Set true to use a config.json, false to use only cmd ling args")
-
-	flag.StringVar(&App.WebSettings.Ip, "webip", "0.0.0.0", "Set the listening IP of the web server.")
-	flag.StringVar(&App.WebSettings.Port, "webport", "8080", "Set the listening Port of the web server.")
-
-	flag.StringVar(&App.RconSettings.Ip, "rconip", "127.0.0.1", "Set the IP of the RCON server to connect to.")
-	flag.StringVar(&App.RconSettings.Port, "rconport", "25575", "Set the port of the RCON server to connect to.")
-	flag.StringVar(&App.RconSettings.Password, "rconpass", "super_secret", "Set the Password of the RCON server.")
-
-	flag.StringVar(&App.DbSettings.Host, "dbhost", "127.0.0.1", "Set the hostname of the DB server to connect to.")
-	flag.StringVar(&App.DbSettings.Port, "dbport", "5432", "Set the Port of the DB server to connect to.")
-	flag.StringVar(&App.DbSettings.User, "dbuser", "postgres", "Set the username of the DB server to connect to.")
-	flag.StringVar(&App.DbSettings.Password, "dbpass", "postgres", "Set the password of the DB server to connect to.")
-	flag.StringVar(&App.DbSettings.DbName, "dbname", "gorcon", "Set the name of the database on the DB server to connect to.")
-
-	fmt.Println(flag.Args())
-	flag.Parse()
-	fmt.Println(flag.Args())
-
-	if !App.Config {
-		fmt.Println("Using Command Line args to configure app.")
-	} else {
-		fmt.Println("Using Config.json to configure app.")
-		setupJsonAppSettings()
-	}
-}
-
-func setupJsonAppSettings() {
-	configFile, err := os.Open("config.json")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer configFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(configFile)
-
-	json.Unmarshal(byteValue, &App)
 }
